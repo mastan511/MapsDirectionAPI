1b1a55f264736e275dbb97add8028db6fce95dd5
Mastan Vali P <mastanvali.apssdc@gmail.com>
2021-02-03 20:01:07 +0530

    Code added

---
+++ DirectionsApp/app/src/main/java/com/example/directionsapp/MapsActivity.java
@@ -0,0 +1,98 @@
+package com.example.directionsapp;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.directionsapp.directonhelpers.FetchURL;
+import com.example.directionsapp.directonhelpers.TaskLoadedCallback;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, TaskLoadedCallback {
+
+    private GoogleMap mMap;
+    private MarkerOptions place1, place2;
+    Button getDirection;
+    private Polyline currentPolyline;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        getDirection = findViewById(R.id.button);
+        place1 = new MarkerOptions().position(new LatLng(15.6319, 77.2759)).title("Location 1");
+        place2 = new MarkerOptions().position(new LatLng(15.8118329,77.9695891)).title("Location 2");
+       getDirection.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               String url = getUrl(place1.getPosition(),place2.getPosition(),"driving");
+               new FetchURL(MapsActivity.this).execute(url, "driving");
+           }
+       });
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+       /* mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+        mMap = googleMap;
+        Log.d("mylog", "Added Markers");
+        mMap.addMarker(place1);
+        mMap.addMarker(place2);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(15.6319, 77.2759),7.0f));
+    }
+
+    @Override
+    public void onTaskDone(Object... values) {
+        if (currentPolyline != null)
+            currentPolyline.remove();
+        currentPolyline = mMap.addPolyline((PolylineOptions) values[0]);
+
+    }
+
+    private String getUrl(LatLng origin, LatLng dest, String directionMode) {
+        // Origin of route
+        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
+        // Destination of route
+        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
+        // Mode
+        String mode = "mode=" + directionMode;
+        // Building the parameters to the web service
+        String parameters = str_origin + "&" + str_dest + "&" + mode;
+        // Output format
+        String output = "json";
+        // Building the url to the web service
+        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters + "&key=" + getString(R.string.google_maps_key);
+        return url;
+    }
+}
